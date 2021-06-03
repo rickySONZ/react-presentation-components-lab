3c38b28946f104bdc09ae1a7ab81f368708c64af
Ryan Erricson <ryanerricson@gmail.com>
2021-06-03 15:40:00 -0400

    commit tree dde11212c5099562d7bed0295ec94cc07286d5ed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent onClick={()=> console.log('hi')} />
   </div>,
   document.getElementById('root')
 );
